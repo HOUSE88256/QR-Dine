2dd27d38fd8b730530625c5c81bb42aa7e813d99
HOUSE88256 <house88creative@gmail.com>
2025-03-30 12:31:31 +0300

    Add files via upload

---
+++ socket-server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  setInterval(() => {
+    socket.emit("new-order", { id: Math.random(), item: "Sample Dish" });
+  }, 10000);
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => {
+  console.log("Socket server running on port", PORT);
+});
